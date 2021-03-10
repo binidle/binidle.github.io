708eb298bc0e701b5fa0d5961e1231c7381b4fab
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2021-03-10 13:23:07 +1100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var player = {
     qlavrams: new Decimal(0),
     cracks: new Decimal(0), 
     bins: [new Bin()],  
-    randForcers: new Decimal(0),   
+    randForcers: new Decimal(0),    
     bruteForcers: new Decimal(0)   
 } 
      
