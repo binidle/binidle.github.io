d67548a14b7d7602825985b837f4171095450832
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2021-03-11 14:56:48 +1100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var player = {
     bins: [new Bin()],   
     randForcers: new Decimal(0),     
     bruteForcers: new Decimal(0),
-	sMultiplier: new Decimal(1),
+	sMultiplier: new Decimal(1), 
 	lowp: false
 }   
       
