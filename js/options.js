8658843382732c2f5690d56b2a3161c02c84a285
lockness-Ko <videolk9373@gmail.com>
2021-03-16 08:31:34 +1100

    Add download function
    , begin implementing save functionality

--- js/options.js
+++ js/options.js
@@ -1,7 +1,29 @@
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+// Start file download.
+// download("hello.txt", "This is the content of my file :)");
+
 function toggleLowP() {
     player.lowp = !playerlowp;
 }
 
 function rainbow() {
     document.body.style.backgroundImage = "url('./rainbow.gif')";
+}
+
+function save(local = true) {
+    if (local) {
+
+    }
 }
