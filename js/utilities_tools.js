de8b513f97fb04e38aa55f1dadf42e88ea5a4f3a
Orliminecraft <45833845+Orliminecraft@users.noreply.github.com>
2021-03-15 20:34:10 +1100

    Update utilities_tools.js

--- js/utilities_tools.js
+++ js/utilities_tools.js
@@ -31,7 +31,7 @@ function updateSolves() {
     document.querySelector("#crackps").innerText = "CPT: " + ((((player.bcracks * (0.025 + (0.005 * (player.cFormula - 1)))) / (1 + (player.cracks + player.bcracks) / 1 + player.bcracks))) * player.cMultiplier).toFixed(5);
     document.querySelector("#version").innerText = "Version: 0.0.1"
 }
-
+ 
 function genBinary(len) {
     n = [];
     for (let i = 0; i < len; i++) {
