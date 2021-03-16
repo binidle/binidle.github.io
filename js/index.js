99d452924ccd6b933296fda9cb5d2e77991f6aef
Orliminecraft <45833845+Orliminecraft@users.noreply.github.com>
2021-03-16 15:20:51 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -96,7 +96,7 @@ function cForm() {
         player.cFormula = player.cFormula.add(1);
         player.cracks = player.cracks.sub(temp);
         temp = (500 * player.cFormula)
-        document.querySelector("#crackFormUPG").innerText = "Buy a Crack Formula Boost (" + numberformat.format(0.005 + (0.001 * (player.cFormula.sub(1)))) + " -> " + numberformat.format(0.005 + (0.001 * (player.cFormula))) + ") " + temp + "Փ" // broken but idc we'll do it later
+        document.querySelector("#crackFormUPG").innerText = "Buy a Crack Formula Boost (" + numberformat.format(0.004 + (0.001 * player.cFormula)) + " -> " + numberformat.format(0.004 + (0.001 * player.cFormula.add(1))) + ") " + temp + "Փ" // broken but idc we'll do it later
     } else {
         alert("You need at least " + temp + "Փ to buy a Crack Formula Boost! You currently have " + player.cracks + "Փ")
     }
