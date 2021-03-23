62c38fa3b7ebafb4d3aa71a0e9f36df3049594fd
lockness-Ko <videolk9373@gmail.com>
2021-03-23 13:02:28 +1100

    stuff

--- js/utilities_tools.js
+++ js/utilities_tools.js
@@ -60,64 +60,64 @@ function updateSolves() {
     document.querySelector("#version").textContent = "Version: b0.2.2 Colour Update!!!!!! real wacky m8; performance fixed; currently in beta";
     document.querySelector("#rforcers").textContent = "Randforcers: " + numberformat.format(player.randForcers);
     document.querySelector("#bforcers").textContent = "Bruteforcers: " + numberformat.format(player.bruteForcers);
-    document.querySelector("#played").textContent = "Time Played: "+ time_ago(started);
-    document.querySelector("#solavg").textContent = "Solve Performance: "+ (1/(solavg/1000)).toFixed(2)+"/s";
+    document.querySelector("#played").textContent = "Time Played: " + time_ago(started);
+    document.querySelector("#solavg").textContent = "Solve Performance: " + (1 / (solavg / 1000)).toFixed(2) + "/s";
 }
 
 function time_ago(time) {
 
     switch (typeof time) {
-      case 'number':
-        break;
-      case 'string':
-        time = +new Date(time);
-        break;
-      case 'object':
-        if (time.constructor === Date) time = time.getTime();
-        break;
-      default:
-        time = +new Date();
+        case 'number':
+            break;
+        case 'string':
+            time = +new Date(time);
+            break;
+        case 'object':
+            if (time.constructor === Date) time = time.getTime();
+            break;
+        default:
+            time = +new Date();
     }
     var time_formats = [
-      [60, 'seconds', 1], // 60
-      [120, '1 minute ago', '1 minute from now'], // 60*2
-      [3600, 'minutes', 60], // 60*60, 60
-      [7200, '1 hour ago', '1 hour from now'], // 60*60*2
-      [86400, 'hours', 3600], // 60*60*24, 60*60
-      [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
-      [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-      [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
-      [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-      [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
-      [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-      [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
-      [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-      [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
-      [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+        [60, 'seconds', 1], // 60
+        [120, '1 minute ago', '1 minute from now'], // 60*2
+        [3600, 'minutes', 60], // 60*60, 60
+        [7200, '1 hour ago', '1 hour from now'], // 60*60*2
+        [86400, 'hours', 3600], // 60*60*24, 60*60
+        [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
+        [604800, 'days', 86400], // 60*60*24*7, 60*60*24
+        [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
+        [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
+        [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
+        [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+        [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
+        [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+        [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
+        [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
     ];
     var seconds = (+new Date() - time) / 1000,
-      token = 'ago',
-      list_choice = 1;
-  
+        token = 'ago',
+        list_choice = 1;
+
     if (seconds == 0) {
-      return 'Just now'
+        return 'Just now'
     }
     if (seconds < 0) {
-      seconds = Math.abs(seconds);
-      token = 'from now';
-      list_choice = 2;
+        seconds = Math.abs(seconds);
+        token = 'from now';
+        list_choice = 2;
     }
     var i = 0,
-      format;
+        format;
     while (format = time_formats[i++])
-      if (seconds < format[0]) {
-        if (typeof format[2] == 'string')
-          return format[list_choice];
-        else
-          return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
-      }
+        if (seconds < format[0]) {
+            if (typeof format[2] == 'string')
+                return format[list_choice];
+            else
+                return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+        }
     return time;
-  }
+}
 
 function genBinary(len) {
     n = [];
@@ -165,7 +165,7 @@ function acheiveBox(msg) {
         zz.className = "alert ach";
         zz.innerHTML = `<span class="closebtn">&times;</span>` + msg + ``;
         document.querySelector("#alertBoxs").appendChild(zz);
-  
+
         zz.onclick = function () {
             div = zz;
             div.style.opacity = "0";
@@ -213,4 +213,14 @@ function bm(m) {
 
     temp = (500 * player.cFormula)
     document.querySelector("#crackFormUPG").innerText = "Buy a Crack Formula Boost (" + numberformat.format(player.cFormula) + "x -> " + numberformat.format(player.cFormula.add(1)) + "x) " + temp * bms + "Փ" // broken but idc we'll do it later
+}
+
+function sigma(start, end, modifier) {
+    const length = end - start + 1;
+    const map = (v, k) => modifier ? modifier(k + start) : k + start;
+    const sum = (a, b) => a + b;
+
+    return Array.from({
+        length
+    }, map).reduce(sum);
 }
