8afafa702351b73b18bcf49c73dd74451d3b1d3d
lockness-Ko <videolk9373@gmail.com>
2021-03-23 12:05:54 +1100

    Resets seed every 11154 iterations (this is the state size of the random generator)

--- js/random.js
+++ js/random.js
@@ -3,11 +3,18 @@ function RNG(seed) {
     this.m = 0x80000000; // 2**31;
     this.a = 1103515245;
     this.c = 12345;
+    this.s=0;
 
     this.state = seed;
     this.nextFloat = function () {
         s = ((this.a * this.state + this.c) % this.m);
         this.state = s;
+        if(this.s>11154){
+            this.state=Math.random();
+            // console.log("a");
+            this.s=0;
+        }
+        this.s++;
         return s / (this.m - 1);
     }
 }
