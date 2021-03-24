10d1ace5a7171808cd9bf2e15e258b8f29271d71
lockness-Ko <videolk9373@gmail.com>
2021-03-24 14:14:13 +1100

    Added load save from clipboard

--- js/options.js
+++ js/options.js
@@ -48,13 +48,12 @@ function save(local = true) {
     abc["binmax"] = player.bins[0].bins.length;
     if (local) {
         download("save.json", enc(btoa(JSON.stringify(abc)) + "|" + checksum(btoa(JSON.stringify(abc))), "bruh funny"));
-    }
-    else {
+    } else {
         navigator.clipboard.writeText(enc(btoa(JSON.stringify(abc)) + "|" + checksum(btoa(JSON.stringify(abc))), "bruh funny"));
     }
 }
 
-function load() { // weedmart calls: THIS DOES NOT WORK YEt
+function load() { // weedmart calls: THIS WORKIS!!
     a = "";
     var pickerOpts = {
         types: [{
@@ -102,20 +101,74 @@ function load() { // weedmart calls: THIS DOES NOT WORK YEt
                 player.bins.splice(j, 1);
             }
 
-            for(i=0;i<decoded.binamt;i++){
+            for (i = 0; i < decoded.binamt; i++) {
                 buyBin(true);
-                for(f=0;f<decoded.bins[i].bins.length;f++){
-                    addBin(Math.round(random.nextFloat()),i);
+                for (f = 0; f < decoded.bins[i].bins.length; f++) {
+                    addBin(Math.round(random.nextFloat()), i);
                 };
             }
             console.log(decoded);
-            for(indexidk=0;indexidk<decoded.bins.length;indexidk++){
-                player.bins[indexidk].randForcing=decoded.bins[indexidk].randForcing;
-                player.bins[indexidk].randForcers=decoded.bins[indexidk].randForcers;
-                player.bins[indexidk].bruteForcing=decoded.bins[indexidk].bruteForcing;
-                player.bins[indexidk].bruteForcers=decoded.bins[indexidk].bruteForcers;
-                player.bins[indexidk].currGoal=decoded.bins[indexidk].currGoal;
+            for (indexidk = 0; indexidk < decoded.bins.length; indexidk++) {
+                player.bins[indexidk].randForcing = decoded.bins[indexidk].randForcing;
+                player.bins[indexidk].randForcers = decoded.bins[indexidk].randForcers;
+                player.bins[indexidk].bruteForcing = decoded.bins[indexidk].bruteForcing;
+                player.bins[indexidk].bruteForcers = decoded.bins[indexidk].bruteForcers;
+                player.bins[indexidk].currGoal = decoded.bins[indexidk].currGoal;
             }
         }
     })));
+}
+
+function loadc() {
+    navigator.clipboard.readText().then(zz => {
+        dec1 = dec(zz).split("|");
+        if (dec1[1] != checksum(dec1[0])) {
+            alertBox("weedmart calls: This save file looks like it has been tAmPeReD with!!");
+        } else {
+            decoded = JSON.parse(atob(dec1[0]));
+            player.solves = new Decimal(parseFloat(decoded.player.solves));
+            player.sMultiplier = new Decimal(parseFloat(decoded.player.sMultiplier));
+            player.qMultiplier = new Decimal(parseFloat(decoded.player.qMultiplier));
+            player.cMultiplier = new Decimal(parseFloat(decoded.player.cMultiplier));
+            player.randForcers = new Decimal(parseFloat(decoded.player.randForcers));
+            player.bruteForcers = new Decimal(parseFloat(decoded.player.bruteForcers));
+            player.qlavrams = new Decimal(parseFloat(decoded.player.qlavrams));
+            player.bcracks = new Decimal(parseFloat(decoded.player.bcracks));
+            player.cracks = new Decimal(parseFloat(decoded.player.cracks));
+
+            player.bins.forEach((i, j) => {
+                i.randForcing = false;
+                i.randForcers = 0;
+                i.bruteForcing = false;
+                i.bruteForcers = 0;
+                i.currGoal = genBinary(1);
+                i.bins.forEach((sz) => {
+                    sz.remove();
+                });
+                i.bins = [];
+
+            }, player);
+            for (let i = document.querySelector("#lines").children.length - 1; i > -1; i--) {
+                document.querySelector("#lines").children[i].remove();
+            }
+            for (let j = 0; j < player.bins.length; j++) {
+                player.bins.splice(j, 1);
+            }
+
+            for (i = 0; i < decoded.binamt; i++) {
+                buyBin(true);
+                for (f = 0; f < decoded.bins[i].bins.length; f++) {
+                    addBin(Math.round(random.nextFloat()), i);
+                };
+            }
+            console.log(decoded);
+            for (indexidk = 0; indexidk < decoded.bins.length; indexidk++) {
+                player.bins[indexidk].randForcing = decoded.bins[indexidk].randForcing;
+                player.bins[indexidk].randForcers = decoded.bins[indexidk].randForcers;
+                player.bins[indexidk].bruteForcing = decoded.bins[indexidk].bruteForcing;
+                player.bins[indexidk].bruteForcers = decoded.bins[indexidk].bruteForcers;
+                player.bins[indexidk].currGoal = decoded.bins[indexidk].currGoal;
+            }
+        }
+    });
 }
